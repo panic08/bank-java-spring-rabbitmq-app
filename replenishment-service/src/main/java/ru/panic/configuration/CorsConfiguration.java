59069fe0c74843e253f6d4fef457f4de3a56fe2b
panic08 <andrey1010102008@gmail.com>
2023-06-01 00:07:52 +0300

    v1.2 added "webhook-receiver" module(fourth)

--- replenishment-service/src/main/java/ru/panic/configuration/CorsConfiguration.java
+++ replenishment-service/src/main/java/ru/panic/configuration/CorsConfiguration.java
@@ -10,6 +10,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/v1/qiwi");
+        registry.addMapping("/api/v1/qiwi")
+                .allowedOrigins(
+                        "http://79.142.16.0/20",
+                        "http://195.189.100.0/22",
+                        "http://91.232.230.0/23",
+                        "http://91.213.51.0/24"
+                );
     }
 }
